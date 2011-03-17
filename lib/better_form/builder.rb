8f2f5a55541ec98550eb3581a5c1cbcec82b34da
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-03-17 15:07:48 +1100

    Added handling so that check_box and radio_button fields have the 'inline' class applied to their labels

--- lib/better_form/builder.rb
+++ lib/better_form/builder.rb
@@ -3,8 +3,8 @@ module BetterForm
     include ActionView::Helpers::UrlHelper
 
     helpers = field_helpers + %w(date_select datetime_select time_select collection_select select time_zone_select) - %w(label hidden_field fields_for)
-    helpers.each do |name|
-      define_method(name) do |field_name, *args|
+    helpers.each do |field_type|
+      define_method(field_type) do |field_name, *args|
         options = args.extract_options!
         validations = generate_validations(@object, field_name)
         options.merge!(validations)
@@ -12,11 +12,11 @@ module BetterForm
         if label == false
           super(field_name, *(args << options))
         elsif label
-          generate_label(field_name, label) + super(field_name, *(args << options))
+          generate_label(field_type, field_name, label) + super(field_name, *(args << options))
         elsif @template.label_all? == false
           super(field_name, *(args << options))
         else
-          generate_label(field_name, label) + super(field_name, *(args << options))
+          generate_label(field_type, field_name, label) + super(field_name, *(args << options))
         end
       end
     end
@@ -32,14 +32,15 @@ module BetterForm
     end
 
   private
-    def generate_label(method, label)
+    def generate_label(field_type, method, label)
       if label == true
         label_text = method.to_s.gsub(/_/, ' ').capitalize
       else
         label_text = label.to_s
       end
 
-      label(method, label_text)
+      options = { :class => :inline } if %w(check_box radio_button).include?(field_type)
+      label(method, label_text, options ||= {})
     end
 
     def generate_validations(object, attribute)
