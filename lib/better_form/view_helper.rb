524193f74b22571e04a635da3600c46de2268497
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-05-23 17:37:53 +1000

    Added the better_form class to forms created using the better_form_for method

--- lib/better_form/view_helper.rb
+++ lib/better_form/view_helper.rb
@@ -2,6 +2,7 @@ module BetterForm
   module ViewHelper
     def better_form_for(record_or_name_or_array, *args, &proc)
       options = args.extract_options!.reverse_merge(:builder => BetterForm::Builder)
+      options[:class] = "#{options[:class]} better_form"
       @label_all = options[:label]
       @validate_all = options[:validate]
       form_for(record_or_name_or_array, *(args << options), &proc)
