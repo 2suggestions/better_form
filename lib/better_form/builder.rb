43c229e7d107aa496974e3d4fda292a11415200e
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-05-23 17:26:33 +1000

    Added a validate option for the whole better_form_for, better_fields_for, or a specific field, allowing validations to be enabled and disabled on a fine-grained basis. Field level options have precedence over form level options

--- lib/better_form/builder.rb
+++ lib/better_form/builder.rb
@@ -5,17 +5,26 @@ module BetterForm
     helpers = field_helpers + %w(date_select datetime_select time_select collection_select select time_zone_select) - %w(label hidden_field fields_for)
     helpers.each do |field_type|
       define_method(field_type) do |field_name, *args|
+        # Extract the options hash and remove the label and validate options
         options = args.extract_options!
-        validations = generate_validations(@object, field_name)
-        options.merge!(validations)
         label = options.delete(:label)
+        validate = options.delete(:validate)
 
-        unless @object.errors[field_name].blank?
-          css_classes = options.delete(:class)
-          options.merge!({ :class => "#{css_classes} invalid" })
-          error_message = generate_error(field_name)
+        # If this field is explicitly validated, or this field and the whole form aren't explicitly *not* validated
+        if validate or (validate != false and @template.validate_all? != false)
+          # Generate validations for the field
+          validations = generate_validations(@object, field_name)
+          options.merge!(validations)
+
+          # Add an 'invalid' class to it if it has errors
+          unless @object.errors[field_name].blank?
+            css_classes = options.delete(:class)
+            options.merge!({ :class => "#{css_classes} invalid" })
+            error_message = generate_error(field_name)
+          end
         end
 
+        # Generate the field, adding a label where appropriate
         if label == false
           super(field_name, *(args << options)) + error_message
         elsif label
