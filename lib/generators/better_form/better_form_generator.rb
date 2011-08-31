a057b8786b93ab400bc19c43ffe54262c7e4fdf3
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-08-31 22:02:25 +1000

    Fixed generator to use thor's copy_file instead of template method

--- lib/generators/better_form/better_form_generator.rb
+++ lib/generators/better_form/better_form_generator.rb
@@ -5,7 +5,7 @@ class BetterFormGenerator < Rails::Generators::Base
 
   desc 'Install the default better_form initializer and partial'
   def generate
-    template "better_form.rb", "config/initializers/better_form.rb"
-    template "field.html.erb", "app/views/better_form/_field.html.erb"
+    copy_file "better_form.rb", "config/initializers/better_form.rb"
+    copy_file "field.html.erb", "app/views/better_form/_field.html.erb"
   end
 end
