90445422e75cb0459de82f08ed465201e7ab1279
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-09-05 16:37:11 +1000

    Created inline_field template and updated initializer and generator to make use of it

--- lib/generators/better_form/better_form_generator.rb
+++ lib/generators/better_form/better_form_generator.rb
@@ -7,5 +7,6 @@ class BetterFormGenerator < Rails::Generators::Base
   def generate
     copy_file "better_form.rb", "config/initializers/better_form.rb"
     copy_file "field.html.erb", "app/views/better_form/_field.html.erb"
+    copy_file "inline_field.html.erb", "app/views/better_form/_inline_field.html.erb"
   end
 end
--- lib/generators/better_form/templates/better_form.rb
+++ lib/generators/better_form/templates/better_form.rb
@@ -1,7 +1,11 @@
 module BetterForm
   class Builder
     def better_form_field(form_field, field_type, generated_label, error_messages, options)
-      content = @template.render(:partial => 'better_form/field', :locals => { :form_field => form_field, :field_type => field_type, :generated_label => generated_label, :error_messages => error_messages, :options => options })
+      if [:radio_button, :check_box].include?(field_type.to_sym)
+        content = @template.render(:partial => 'better_form/inline_field', :locals => { :form_field => form_field, :field_type => field_type, :generated_label => generated_label, :error_messages => error_messages, :options => options })
+      else
+        content = @template.render(:partial => 'better_form/field', :locals => { :form_field => form_field, :field_type => field_type, :generated_label => generated_label, :error_messages => error_messages, :options => options })
+      end
       content
     end
   end
